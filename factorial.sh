c86ceeaf96fd12c7e76cba7715ff858725ba8e5a
Peter Andrews-Briscoe <peterandrews-briscoe@Peters-MacBook-Pro.local>
2024-10-06 19:47:31 +0100

    added first attempt at error handling for factorials being less than 1

--- factorial.sh
+++ factorial.sh
@@ -2,13 +2,21 @@
 
 factorial(){
     local num=$1
-    local a=1
-    while (($num > 1))
-        do
-        a=$(($a * $num))
-        ((num=$num-1))
-        done
-    echo $a
+    if (($num > 1)); then
+        local a=1
+        while (($num > 1))
+            do
+            a=$(($a * $num))
+            ((num=$num-1))
+            done
+    elif (($num = 1))
+    then
+        local a=1
+    elif (($num < 1))
+    then
+        a="the input must be higher than 1"
+    fi
+        echo $a
 }
 
 
