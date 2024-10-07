3b22273f87c48eec470150b0e9f74f090de64884
Peter Andrews-Briscoe <peterandrews-briscoe@Peters-MacBook-Pro.local>
2024-10-07 11:58:42 +0100

    uploading complete exception handling

--- factorial.sh
+++ factorial.sh
@@ -1,27 +1,22 @@
 #!/bin/bash
 
 factorial(){
-    if [$# -gt 1]; then
-        echo "too many parameters"
-    elif [$# -lt 1]; then
-        echo "not enough parameters"
-    else
-        local num=$1
-        if (($num > 1)); then
-            local a=1
-            while (($num > 1))
-                do
-                a=$(($a * $num))
-                ((num=$num-1))
-                done
-        elif (($num = 1))
-        then
-            local a=1
-        elif (($num < 1))
-        then
-            a="the input must be higher than 1"
-        fi
-            echo $a
+    local num=$1
+    if (($num > 1)); then
+        local a=1
+        while (($num > 1))
+            do
+            a=$(($a * $num))
+            ((num=$num-1))
+            done
+    elif (($num = 1))
+    then
+        local a=1
+    elif (($num < 1))
+    then
+        a="the input must be higher than 1"
+    fi
+        echo $a
 }
 
 
